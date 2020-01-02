15fe0e57e027e78842f3624a6de64261d5e8d85d
oneRain <onerain88@gmail.com>
2020-01-02 10:37:51 +0800

    chore: 完善 http 请求日志输出

--- lib/internal/http/lc_http_client.dart
+++ lib/internal/http/lc_http_client.dart
@@ -9,40 +9,7 @@ class LCHttpClient {
   
   LCHttpClient(this.appId, this.appKey, this.authority);
 
-  Future<Map<String, dynamic>> post(String path, { Map<String, dynamic> data }) async {
-    var client = new HttpClient();
-    var uri = new Uri.https(authority, path);
-    var request = await client.postUrl(uri);
-    request.headers
-      ..add('X-LC-Id', appId)
-      ..add('X-LC-Key', appKey)
-      ..add('Content-Type', MediaType);
-    if (data != null) {
-      request.write(jsonEncode(data));
-    }
-    var response = await request.close();
-    var body = await response.transform(utf8.decoder).join();
-    // TODO
-
-    Map<String, dynamic> result = jsonDecode(body);
-    return result;
-  }
-
-  Future<Map<String, dynamic>> getObject(String path, String objectId) async {
-    var client = new HttpClient();
-    var uri = new Uri.https(authority, '$path/$objectId');
-    var request = await client.getUrl(uri);
-    request.headers
-      ..add('X-LC-Id', appId)
-      ..add('X-LC-Key', appKey)
-      ..add('Content-Type', MediaType);
-    var response = await request.close();
-    var body = await response.transform(utf8.decoder).join();
-
-    Map<String, dynamic> result = jsonDecode(body);
-    return result;
-  }
-
+  /// 发送 http 请求
   Future<T> send<T>(LCHttpRequest request) async {
     var client = new HttpClient();
     print(authority);
@@ -69,21 +36,23 @@ class LCHttpClient {
       ..add('X-LC-Id', appId)
       ..add('X-LC-Key', appKey)
       ..add('Content-Type', MediaType);
-    print('----------- Http Request Start ------------');
-    
-    print(req.uri);
+    print('=== Http Request Start ===');
+    print('URL: ${req.uri}');
+    print('Method: ${req.method}');
+    print('Headers: ${req.headers}');
     if (request.data != null) {
       String content = jsonEncode(LCEncoder.encodeMap(request.data));
       print(content);
       req.write(content);
     }
-    print('----------- Http Request End --------------');
-    var response = await req.close();
+    print('=== Http Request End =====');
+    HttpClientResponse response = await req.close();
     var body = await response.transform(utf8.decoder).join();
     T result = jsonDecode(body);
-    print('----------- Http Response Start -----------');
-    print(result);
-    print('----------- Http Response End -------------');
+    print('=== Http Response Start ===');
+    print('Status Code: ${response.statusCode}');
+    print('Content: $result');
+    print('=== Http Response End =====');
     return result;
   }
 }
