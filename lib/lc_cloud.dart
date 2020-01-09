75683783a91f32feca71005c7dac38aceb14185d
oneRain <onerain88@gmail.com>
2020-01-09 12:37:24 +0800

    chore: 支持云函数

--- lib/lc_cloud.dart
+++ lib/lc_cloud.dart
@@ -3,11 +3,14 @@ part of leancloud_storage;
 /// 云引擎
 class LCCloud {
   /// 调用云函数
-  static Future<T> callFunction<T>(String name, Map<String, dynamic> params) {
-
+  static Future run(String name, { Map<String, dynamic> params }) async {
+    String path = 'functions/$name';
+    return await LeanCloud._httpClient.post(path, data: params);
   }
 
-  static Future<T> rpcFunction<T>(String name, dynamic params) {
-
+  static Future rpc(String name, { Map<String, dynamic> params }) async {
+    String path = 'call/$name';
+    Map response = await LeanCloud._httpClient.post(path, data: params);
+    return LCDecoder.decode(response);
   }
 }
