6d2d71a99ad60096f0d18e05bf2081ccf82dfdea
oneRain <onerain88@gmail.com>
2020-01-20 11:48:45 +0800

    chore: 简化单元测试的初始化

--- test/object_test.dart
+++ test/object_test.dart
@@ -4,125 +4,121 @@ import 'package:leancloud_storage/leancloud.dart';
 import 'utils.dart';
 
 void main() {
-  test('create object', () async {
-    initNorthChina();
-    LCObject object = new LCObject('Hello');
-    object['intValue'] = 123;
-    object['boolValue'] = true;
-    object['stringValue'] = 'hello, world';
-    object['time'] = DateTime.now();
-    object['intList'] = [1, 1, 2, 3, 5, 8];
-    object['stringMap'] = {
-      'k1': 111,
-      'k2': true,
-      'k3': 'haha'
-    };
-    LCObject nestedObj = new LCObject('World');
-    nestedObj['content'] = '7788';
-    object['objectValue'] = nestedObj;
-    object['pointerList'] = [new LCObject('World'), nestedObj];
-    await object.save();
+  group('object', () {
+    setUp(() => initNorthChina());
 
-    print(object.className);
-    print(object.objectId);
-    print(object.createdAt);
-    print(object.updatedAt);
-    print(object['intValue']);
-    print(object['boolValue']);
-    print(object['stringValue']);
-    print(object['objectValue']);
-    print(object['time']);
+    test('create object', () async {
+      LCObject object = new LCObject('Hello');
+      object['intValue'] = 123;
+      object['boolValue'] = true;
+      object['stringValue'] = 'hello, world';
+      object['time'] = DateTime.now();
+      object['intList'] = [1, 1, 2, 3, 5, 8];
+      object['stringMap'] = {
+        'k1': 111,
+        'k2': true,
+        'k3': 'haha'
+      };
+      LCObject nestedObj = new LCObject('World');
+      nestedObj['content'] = '7788';
+      object['objectValue'] = nestedObj;
+      object['pointerList'] = [new LCObject('World'), nestedObj];
+      await object.save();
 
-    assert(nestedObj == object['objectValue']);
-    print(nestedObj.className);
-    print(nestedObj.objectId);
+      print(object.className);
+      print(object.objectId);
+      print(object.createdAt);
+      print(object.updatedAt);
+      print(object['intValue']);
+      print(object['boolValue']);
+      print(object['stringValue']);
+      print(object['objectValue']);
+      print(object['time']);
 
-    assert(object.objectId != null);
-    assert(object.className != null);
-    assert(object.createdAt != null);
-    assert(object.updatedAt != null);
-    assert(object['intValue'] == 123);
-    assert(object['boolValue'] == true);
-    assert(object['stringValue'] == 'hello, world');
+      assert(nestedObj == object['objectValue']);
+      print(nestedObj.className);
+      print(nestedObj.objectId);
 
-    assert(nestedObj != null);
-    assert(nestedObj.className != null);
-    assert(nestedObj.objectId != null);
-    assert(nestedObj.createdAt != null);
-    assert(nestedObj.updatedAt != null);
+      assert(object.objectId != null);
+      assert(object.className != null);
+      assert(object.createdAt != null);
+      assert(object.updatedAt != null);
+      assert(object['intValue'] == 123);
+      assert(object['boolValue'] == true);
+      assert(object['stringValue'] == 'hello, world');
 
-    object['pointerList'].forEach((item) {
-      assert(item.objectId != null);
+      assert(nestedObj != null);
+      assert(nestedObj.className != null);
+      assert(nestedObj.objectId != null);
+      assert(nestedObj.createdAt != null);
+      assert(nestedObj.updatedAt != null);
+
+      object['pointerList'].forEach((item) {
+        assert(item.objectId != null);
+      });
     });
-  });
 
-  test('save all', () async {
-    initNorthChina();
-    List<LCObject> list = new List<LCObject>();
-    for (int i = 0; i < 5; i++) {
+    test('save all', () async {
+      List<LCObject> list = new List<LCObject>();
+      for (int i = 0; i < 5; i++) {
+        LCObject world = new LCObject('World');
+        world['content'] = 'word_$i';
+        list.add(world);
+      }
+      await LCObject.saveAll(list);
+      list.forEach((item) {
+        assert(item.objectId != null);
+      });
+    });
+
+    test('delete', () async {
       LCObject world = new LCObject('World');
-      world['content'] = 'word_$i';
-      list.add(world);
-    }
-    await LCObject.saveAll(list);
-    list.forEach((item) {
-      assert(item.objectId != null);
+      await world.save();
+      await world.delete();
     });
-  });
 
-  test('delete', () async {
-    initNorthChina();
-    LCObject world = new LCObject('World');
-    await world.save();
-    await world.delete();
-  });
+    test('delete all', () async {
+      List<LCObject> list = [new LCObject('World'), new LCObject('World'), new LCObject('World'), new LCObject('World')];
+      await LCObject.saveAll(list);
+      await LCObject.deleteAll(list);
+    });
 
-  test('delete all', () async {
-    initNorthChina();
-    List<LCObject> list = [new LCObject('World'), new LCObject('World'), new LCObject('World'), new LCObject('World')];
-    await LCObject.saveAll(list);
-    await LCObject.deleteAll(list);
-  });
+    test('fetch', () async {
+      LCQuery<LCObject> query = new LCQuery<LCObject>('Hello');
+      LCObject hello = await query.get('5e14392743c257006fb769d5');
+      print(hello);
+      await hello.fetch(includes: ['objectValue']);
+      LCObject world = hello['objectValue'];
+      print(world['content']);
+    });
 
-  test('fetch', () async {
-    initNorthChina();
-    LCQuery<LCObject> query = new LCQuery<LCObject>('Hello');
-    LCObject hello = await query.get('5e14392743c257006fb769d5');
-    print(hello);
-    await hello.fetch(includes: ['objectValue']);
-    LCObject world = hello['objectValue'];
-    print(world['content']);
-  });
+    test('save with options', () async {
+      LCObject account = new LCObject('Account');
+      account['balance'] = 10;
+      await account.save();
 
-  test('save with options', () async {
-    initNorthChina();
+      account['balance'] = 1000;
+      LCQuery<LCObject> q = new LCQuery('Account');
+      q.whereGreaterThan('balance', 100);
+      try {
+        await account.save(fetchWhenSave: true, query: q);
+      } on LCException catch (e) {
+        print('${e.code} : ${e.message}');
+        assert(e.code == 305);
+      }
+    });
 
-    LCObject account = new LCObject('Account');
-    account['balance'] = 10;
-    await account.save();
+    test('unset', () async {
+      LCObject hello = new LCObject('Hello');
+      hello['content'] = 'hello, world';
+      await hello.save();
+      print(hello['content']);
+      assert(hello['content'] == 'hello, world');
 
-    account['balance'] = 1000;
-    LCQuery<LCObject> q = new LCQuery('Account');
-    q.whereGreaterThan('balance', 100);
-    try {
-      await account.save(fetchWhenSave: true, query: q);
-    } on LCException catch (e) {
-      print('${e.code} : ${e.message}');
-      assert(e.code == 305);
-    }
-  });
-
-  test('unset', () async {
-    initNorthChina();
-    LCObject hello = new LCObject('Hello');
-    hello['content'] = 'hello, world';
-    await hello.save();
-    print(hello['content']);
-    assert(hello['content'] == 'hello, world');
-
-    hello.unset('content');
-    await hello.save();
-    print(hello['content']);
-    assert(hello['content'] == null);
+      hello.unset('content');
+      await hello.save();
+      print(hello['content']);
+      assert(hello['content'] == null);
+    });
   });
 }
--- test/user_test.dart
+++ test/user_test.dart
@@ -4,171 +4,152 @@ import 'package:leancloud_storage/leancloud.dart';
 import 'utils.dart';
 
 void main() {
-  test('sign up', () async {
-    initNorthChina();
-    LCUser user = new LCUser();
-    user.username = DateTime.now().millisecondsSinceEpoch.toString();
-    user.password = 'world';
-    String email = '${DateTime.now().millisecondsSinceEpoch}@qq.com';
-    user.email = email;
-    await user.signUp();
+  group('user', () {
+    setUp(() => initNorthChina());
 
-    print(user.username);
-    print(user.password);
+    test('sign up', () async {
+      LCUser user = new LCUser();
+      user.username = DateTime.now().millisecondsSinceEpoch.toString();
+      user.password = 'world';
+      String email = '${DateTime.now().millisecondsSinceEpoch}@qq.com';
+      user.email = email;
+      await user.signUp();
 
-    assert(user.objectId != null);
-    print(user.objectId);
-    assert(user.sessionToken != null);
-    print(user.sessionToken);
-    assert(user.email == email);
-  });
+      print(user.username);
+      print(user.password);
 
-  test('login', () async {
-    initNorthChina();
-    await LCUser.login('hello', 'world');
-    LCUser current = LCUser.currentUser;
-    assert(current.objectId != null);
-    assert(!current.emailVerified);
-    assert(!current.mobileVerified);
-    assert(current.mobile == '15101006008');
-  });
+      assert(user.objectId != null);
+      print(user.objectId);
+      assert(user.sessionToken != null);
+      print(user.sessionToken);
+      assert(user.email == email);
+    });
 
-  // test('login by mobile', () async {
-  //   initNorthChina();
-  //   await LCUser.loginByMobilePhoneNumber('15101006007', '112358');
-  //   LCUser current = LCUser.currentUser;
-  //   assert(current.objectId != null);
-  // });
+    test('login', () async {
+      await LCUser.login('hello', 'world');
+      LCUser current = LCUser.currentUser;
+      assert(current.objectId != null);
+      assert(!current.emailVerified);
+      assert(!current.mobileVerified);
+      assert(current.mobile == '15101006008');
+    });
 
-  test('login by email', () async {
-    initNorthChina();
-    await LCUser.loginByEmail('171253484@qq.com', 'world');
-    LCUser current = LCUser.currentUser;
-    assert(current.objectId != null);
-  });
+    // test('login by mobile', () async {
+    //   await LCUser.loginByMobilePhoneNumber('15101006007', '112358');
+    //   LCUser current = LCUser.currentUser;
+    //   assert(current.objectId != null);
+    // });
 
-  // test('login by sms code', () async {
-  //   initNorthChina();
-  //   await LCUser.loginBySMSCode('15101006007', '882586');
-  //   LCUser current = LCUser.currentUser;
-  //   assert(current.objectId != null);
-  // });
+    test('login by email', () async {
+      await LCUser.loginByEmail('171253484@qq.com', 'world');
+      LCUser current = LCUser.currentUser;
+      assert(current.objectId != null);
+    });
 
-  // test('request login sms code', () async {
-  //   initNorthChina();
-  //   await LCUser.requestLogionSMSCode('15101006007');
-  // });
+    // test('login by sms code', () async {
+    //   await LCUser.loginBySMSCode('15101006007', '882586');
+    //   LCUser current = LCUser.currentUser;
+    //   assert(current.objectId != null);
+    // });
 
-  // test('login by sms code', () async {
-  //   initNorthChina();
-  //   await LCUser.loginBySMSCode('15101006007', '925752');l
-  // });
+    // test('request login sms code', () async {
+    //   await LCUser.requestLogionSMSCode('15101006007');
+    // });
 
-  test('login by session token', () async {
-    initNorthChina();
-    String sessionToken = 'luo2fpl4qij2050e7enqfz173';
-    await LCUser.becomeWithSessionToken(sessionToken);
-    LCUser current = LCUser.currentUser;
-    assert(current.objectId != null);
-  });
+    // test('login by sms code', () async {
+    //   await LCUser.loginBySMSCode('15101006007', '925752');l
+    // });
 
-  // test('request mobile verify', () async {
-  //   initNorthChina();
-  //   await LCUser.requestMobilePhoneVerify('15101006007');
-  // });
+    test('login by session token', () async {
+      String sessionToken = 'luo2fpl4qij2050e7enqfz173';
+      await LCUser.becomeWithSessionToken(sessionToken);
+      LCUser current = LCUser.currentUser;
+      assert(current.objectId != null);
+    });
 
-  // test('verify mobile', () async {
-  //   initNorthChina();
-  //   await LCUser.becomeWithSessionToken('sbhavbefqk2jc3wgfop3i6om0');
-  //   await LCUser.verifyMobilePhone('944616');
-  // });
+    // test('request mobile verify', () async {
+    //   await LCUser.requestMobilePhoneVerify('15101006007');
+    // });
 
-  // test('request email verify', () async {
-  //   initNorthChina();
-  //   await LCUser.requestEmailVerify('171253484@qq.com');
-  // });
+    // test('verify mobile', () async {
+    //   await LCUser.becomeWithSessionToken('sbhavbefqk2jc3wgfop3i6om0');
+    //   await LCUser.verifyMobilePhone('944616');
+    // });
 
-  // test('request reset password by sms code', () async {
-  //   initNorthChina();
-  //   await LCUser.requestPasswordRestBySmsCode('15101006007');
-  // });
+    // test('request email verify', () async {
+    //   await LCUser.requestEmailVerify('171253484@qq.com');
+    // });
 
-  // test('reset password by sms code', () async {
-  //   initNorthChina();
-  //   await LCUser.resetPasswordBySmsCode('15101006007', '286436', '112358');
-  // });
+    // test('request reset password by sms code', () async {
+    //   await LCUser.requestPasswordRestBySmsCode('15101006007');
+    // });
 
-  test('relate object', () async {
-    initNorthChina();
-    await LCUser.loginByMobilePhoneNumber('15101006007', '112358');
-    LCObject account = new LCObject('Account');
-    account['user'] = LCUser.currentUser;
-    await account.save();
-  });
+    // test('reset password by sms code', () async {
+    //   await LCUser.resetPasswordBySmsCode('15101006007', '286436', '112358');
+    // });
 
-  test('login anonymous', () async {
-    initNorthChina();
-    await LCUser.loginAnonymously();
-  });
+    test('relate object', () async {
+      await LCUser.loginByMobilePhoneNumber('15101006007', '112358');
+      LCObject account = new LCObject('Account');
+      account['user'] = LCUser.currentUser;
+      await account.save();
+    });
 
-  test('login with auth data', () async {
-    initNorthChina();
-  
-    Map<String, dynamic> authData = {
-      'expires_in': 7200,
-      'openid': '${DateTime.now().millisecondsSinceEpoch}',
-      'access_token': '${DateTime.now().millisecondsSinceEpoch}'
-    };
-    await LCUser.loginWithAuthData(authData, 'weixin');
-    print(LCUser.currentUser.sessionToken);
-    assert(LCUser.currentUser.sessionToken != null);
-    String userId = LCUser.currentUser.objectId;
-    print('userId: $userId');
-    print(LCUser.currentUser.authData);
+    test('login anonymous', () async {
+      await LCUser.loginAnonymously();
+    });
 
-    LCUser.logout();
-    assert(LCUser.currentUser == null);
+    test('login with auth data', () async {
+      Map<String, dynamic> authData = {
+        'expires_in': 7200,
+        'openid': '${DateTime.now().millisecondsSinceEpoch}',
+        'access_token': '${DateTime.now().millisecondsSinceEpoch}'
+      };
+      await LCUser.loginWithAuthData(authData, 'weixin');
+      print(LCUser.currentUser.sessionToken);
+      assert(LCUser.currentUser.sessionToken != null);
+      String userId = LCUser.currentUser.objectId;
+      print('userId: $userId');
+      print(LCUser.currentUser.authData);
 
-    await LCUser.loginWithAuthData(authData, 'weixin');
-    print(LCUser.currentUser.sessionToken);
-    assert(LCUser.currentUser.sessionToken != null);
-    assert(LCUser.currentUser.objectId == userId);
-    print(LCUser.currentUser.authData);
-  });
-  
-  test('associate auth data', () async {
-    initNorthChina();
+      LCUser.logout();
+      assert(LCUser.currentUser == null);
 
-    await LCUser.login('hello', 'world');
-    Map<String, dynamic> authData = {
-      'expires_in': 7200,
-      'openid': '${DateTime.now().millisecondsSinceEpoch}',
-      'access_token': '${DateTime.now().millisecondsSinceEpoch}'
-    };
-    await LCUser.currentUser.associateAuthData(authData, 'weixin');
-    print(LCUser.currentUser.authData);
-    print(LCUser.currentUser.authData['weixin']);
-  });
+      await LCUser.loginWithAuthData(authData, 'weixin');
+      print(LCUser.currentUser.sessionToken);
+      assert(LCUser.currentUser.sessionToken != null);
+      assert(LCUser.currentUser.objectId == userId);
+      print(LCUser.currentUser.authData);
+    });
+    
+    test('associate auth data', () async {
+      await LCUser.login('hello', 'world');
+      Map<String, dynamic> authData = {
+        'expires_in': 7200,
+        'openid': '${DateTime.now().millisecondsSinceEpoch}',
+        'access_token': '${DateTime.now().millisecondsSinceEpoch}'
+      };
+      await LCUser.currentUser.associateAuthData(authData, 'weixin');
+      print(LCUser.currentUser.authData);
+      print(LCUser.currentUser.authData['weixin']);
+    });
 
-  test('disassociate auth data', () async {
-    initNorthChina();
+    test('disassociate auth data', () async {
+      await LCUser.login('hello', 'world');
+      await LCUser.currentUser.disassociateWithAuthData('weixin');
+    });
 
-    await LCUser.login('hello', 'world');
-    await LCUser.currentUser.disassociateWithAuthData('weixin');
-  });
+    test('is authenticated', () async {
+      await LCUser.login('hello', 'world');
+      bool isAuthenticated = await LCUser.currentUser.isAuthenticated();
+      print(isAuthenticated);
+      assert(isAuthenticated);
+    });
 
-  test('is authenticated', () async {
-    initNorthChina();
-    await LCUser.login('hello', 'world');
-    bool isAuthenticated = await LCUser.currentUser.isAuthenticated();
-    print(isAuthenticated);
-    assert(isAuthenticated);
-  });
-
-  test('update password', () async {
-    initNorthChina();
-    await LCUser.login('hello', 'world');
-    await LCUser.currentUser.updatePassword('world', 'newWorld');
-    await LCUser.currentUser.updatePassword('newWorld', 'world');
+    test('update password', () async {
+      await LCUser.login('hello', 'world');
+      await LCUser.currentUser.updatePassword('world', 'newWorld');
+      await LCUser.currentUser.updatePassword('newWorld', 'world');
+    });
   });
 }
