201fc21701b5757fc776761da7dc46b2c64b9d56
oneRain <onerain88@gmail.com>
2020-06-28 16:36:01 +0800

    chore: 支持设置云引擎环境

--- lib/lc_cloud.dart
+++ lib/lc_cloud.dart
@@ -2,12 +2,20 @@ part of leancloud_storage;
 
 /// 云引擎
 class LCCloud {
+  static bool isProd = true;
+
+  static void setProduction(bool prod) {
+    isProd = prod;
+  }
+
   /// 调用云函数，结果为 Map 类型
   /// [name] 函数名称
   /// [params] 函数参数
   static Future run(String name, {Map<String, dynamic> params}) async {
     String path = 'functions/$name';
-    return await LeanCloud._httpClient.post(path, data: params);
+    Map<String, dynamic> headers = {'X-LC-Prod': isProd ? 1 : 0};
+    return await LeanCloud._httpClient
+        .post(path, headers: headers, data: params);
   }
 
   /// RPC 调用云函数，结果反序列化为相应对象
@@ -15,7 +23,9 @@ class LCCloud {
   /// [params] 函数参数
   static Future rpc(String name, {Map<String, dynamic> params}) async {
     String path = 'call/$name';
-    Map response = await LeanCloud._httpClient.post(path, data: params);
+    Map<String, dynamic> headers = {'X-LC-Prod': isProd ? 1 : 0};
+    Map response =
+        await LeanCloud._httpClient.post(path, headers: headers, data: params);
     return _LCDecoder.decode(response);
   }
 }
